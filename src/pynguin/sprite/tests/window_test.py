dd9a5103ddd2efe2aae0937109e96e4748e1bac1
Niriel <daneel_libre@yahoo.fr>
2011-01-07 14:12:21 +0100

    Made the window sprite unit test more PyLint-friendly.

--- src/pynguin/sprite/tests/window_test.py
+++ src/pynguin/sprite/tests/window_test.py
@@ -1,41 +1,51 @@
-'''
+"""
 Created on Nov 27, 2010
 
 @author: Niriel
-'''
+"""
 import unittest
 import pygame
-from gui.sprite import guisprite, window
+from pynguin.sprite import guisprite, window
 
-class windowTest(unittest.TestCase):
+# pylint: disable-msg=R0904
+# Because unit tests have tons of public methods and that's normal. 
+
+# pylint: disable-msg=W0212
+# Disabling the warning about calling a 'protected' member of the class (name
+# starting with underscore).  I don't care, I'm allowed to do that in a test
+# suite.
+
+class WindowTest(unittest.TestCase):
+    """Test the sprite.window module."""
     def testInit(self):
-        """window.__init__ does its job."""
-        w = window.Window()
-        self.assertEquals(w.sprites_group.__class__.__name__, 'LayeredUpdates')
-    
+        """Window.__init__ does its job."""
+        win = window.Window()
+        self.assertEquals(win._sprites_group.__class__.__name__,
+                          'LayeredUpdates')
+
     def testGetSpritesAt(self):
-        """window.getSpritesAt return clicked children and itself."""
-        w1 = window.Window()
-        c1 = guisprite.GuiSprite()
-        c2 = guisprite.GuiSprite()
-        w2 = window.Window()
-        c3 = guisprite.GuiSprite()
+        """Window.getSpritesAt return clicked children and itself."""
+        win1 = window.Window()
+        spr1 = guisprite.GuiSprite()
+        spr2 = guisprite.GuiSprite()
+        win2 = window.Window()
+        spr3 = guisprite.GuiSprite()
         #
-        w1.rect = pygame.Rect((200, 100), (300, 150))
-        c1.rect = pygame.Rect((10, 20), (50, 30))
-        c2.rect = pygame.Rect((20, 30), (50, 30))
-        w2.rect = pygame.Rect((240, 90), (50, 50))
-        c3.rect = pygame.Rect((10, 10), (10, 10))
-        # Notice that c1 and c2 overlap.
-        w1.sprites_group.add([c1, c2, w2])
-        w2.sprites_group.add(c3)
+        win1.rect = pygame.Rect((200, 100), (300, 150))
+        spr1.rect = pygame.Rect((10, 20), (50, 30))
+        spr2.rect = pygame.Rect((20, 30), (50, 30))
+        win2.rect = pygame.Rect((240, 90), (50, 50))
+        spr3.rect = pygame.Rect((10, 10), (10, 10))
+        # Notice that spr1 and spr2 overlap.
+        win1._sprites_group.add([spr1, spr2, win2])
+        win2._sprites_group.add(spr3)
         #
-        self.assertEquals(w1.getSpritesAt((0, 0)), [])
-        self.assertEquals(w1.getSpritesAt((200, 100)), [w1])
-        self.assertEquals(w1.getSpritesAt((215, 125)), [w1, c1])
-        self.assertEquals(w1.getSpritesAt((225, 135)), [w1, c1, c2])
-        self.assertEquals(w1.getSpritesAt((440, 190)), [w1, w2])
-        self.assertEquals(w1.getSpritesAt((450, 200)), [w1, w2, c3])
+        self.assertEquals(win1.getSpritesAt((0, 0)), [])
+        self.assertEquals(win1.getSpritesAt((200, 100)), [win1])
+        self.assertEquals(win1.getSpritesAt((215, 125)), [win1, spr1])
+        self.assertEquals(win1.getSpritesAt((225, 135)), [win1, spr1, spr2])
+        self.assertEquals(win1.getSpritesAt((440, 190)), [win1, win2])
+        self.assertEquals(win1.getSpritesAt((450, 200)), [win1, win2, spr3])
 
 if __name__ == '__main__':
     unittest.main()
