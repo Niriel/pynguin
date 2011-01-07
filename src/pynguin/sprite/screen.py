4ad1d5412edd9052b0d874e3b54202392232e719
Niriel <daneel_libre@yahoo.fr>
2011-01-07 14:23:27 +0100

    Remove a print statement used for debugging.

--- src/pynguin/sprite/screen.py
+++ src/pynguin/sprite/screen.py
@@ -1,8 +1,8 @@
-'''
+"""
 Created on Nov 27, 2010
 
 @author: Niriel
-'''
+"""
 
 import pygame
 from window import Window
@@ -10,9 +10,12 @@ from window import Window
 __all__ = ['Screen']
 
 class Screen(Window):
+    """A screen is a window that takes the display as drawing surface."""
+
     BG_COLOR = (255, 255, 255)
+
     def _createImage(self):
+        """Set the display surface as its own surface."""
         self.image = pygame.display.get_surface()
         self.rect = self.image.get_rect()
-        print self.rect
         self.drawable_image = self.image
