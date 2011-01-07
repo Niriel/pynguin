04e60a0a9e9cc499c4d726773deb6c0c09f2921d
Niriel <daneel_libre@yahoo.fr>
2011-01-07 13:52:57 +0100

    Added setMode method to Button sprite to switch between pressed, normal...

--- src/pynguin/sprite/button.py
+++ src/pynguin/sprite/button.py
@@ -1,8 +1,9 @@
-'''
+#! /usr/bin/python
+"""
 Created on Dec 2, 2010
 
 @author: Niriel
-'''
+"""
 
 from guisprite import GuiSprite
 import rectangle
@@ -10,6 +11,11 @@ import rectangle
 __all__ = ['Button']
 
 class Button(GuiSprite):
+    """A Button is a rectangular sprite that looks like it can be pressed.
+
+    Its appearance is set by the setMode method.
+
+    """
     BG_COLOR_NORMAL_TOP = (240, 240, 240)
     BG_COLOR_NORMAL_BOTTOM = (230, 230, 230)
     BG_COLOR_HIGHLIGHT_TOP = (250, 250, 250)
@@ -17,26 +23,46 @@ class Button(GuiSprite):
     BG_COLOR_PRESSED = (220, 220, 220)
     BG_COLOR_INACTIVE = (200, 200, 200)
     BD_COLOR = (100, 100, 100)
-    def __init__(self):
-        GuiSprite.__init__(self)
-        self._drawBackground = self._drawNormal
 
-    def _draw(self):
-        self._drawBackground()
-        rectangle.Border(self.drawable_image, self.BD_COLOR, 2)
+    def __init__(self):
+        """Initialize a new button.
+        
+        The mode is set to 'normal'.
+        
+        """
+        GuiSprite.__init__(self)
+        self._draw_function = None
+        self.setMode('normal')
 
     def _drawNormal(self):
+        """Draw a normal button."""
         rectangle.RectangleVGrad(self.drawable_image,
                                  self.BG_COLOR_NORMAL_TOP,
                                  self.BG_COLOR_NORMAL_BOTTOM)
+        rectangle.Border(self.drawable_image, self.BD_COLOR, 2)
 
-    def _drawHighlight(self):
+    def _drawHighlighted(self):
+        """Draw a highlighted button."""
         rectangle.RectangleVGrad(self.drawable_image,
                                  self.BG_COLOR_HIGHLIGHT_TOP,
                                  self.BG_COLOR_HIGHLIGHT_BOTTOM)
+        rectangle.Border(self.drawable_image, self.BD_COLOR, 2)
 
     def _drawPressed(self):
+        """Draw a pressed button."""
         self.drawable_image.fill(self.BG_COLOR_PRESSED)
- 
+        rectangle.Border(self.drawable_image, self.BD_COLOR, 2)
+
     def _drawInactive(self):
+        """Draw an inactive button."""
         self.drawable_image.fill(self.BG_COLOR_INACTIVE)
+        rectangle.Border(self.drawable_image, self.BD_COLOR, 2)
+
+    def _draw(self):
+        """Draw the button."""
+        # There is no need to call GuiSprite._draw.
+        self._draw_function()
+
+    def setMode(self, mode):
+        """Set a mode: 'normal', 'highlighted', 'pressed', 'inactive'."""
+        self._draw_function = getattr('_draw%s' % mode.title())
