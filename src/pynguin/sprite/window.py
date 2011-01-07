88aed5f664eff5c94eff14f1b0835af37f7487cb
Niriel <daneel_libre@yahoo.fr>
2011-01-07 14:31:06 +0100

    Added convenience method for managing the layers.

--- src/pynguin/sprite/window.py
+++ src/pynguin/sprite/window.py
@@ -1,13 +1,23 @@
-'''
+#! /usr/bin/python
+"""
 Created on Nov 26, 2010
 
 @author: Niriel
-'''
+"""
 
 import pygame
 from guisprite import GuiSprite
 
 __all__ = ['Window']
+
+class WindowError(RuntimeError):
+    """Base class for exceptions in the window module."""
+
+class SpriteNotInGroupError(WindowError):
+    """Raised when removing a sprite that is not part of the group."""
+
+class SpriteAlreadyInGroupError(WindowError):
+    """Raised when adding a sprite that was already added."""
 
 class Window(GuiSprite):
     """A Window is a Sprite that draws other sprites onto itself.
@@ -17,50 +27,85 @@ class Window(GuiSprite):
 
     This is handy:
 
-    - since the window has its own surface we make sure we never draw outside
-    of the window ;
-
-    - when a LayeredUpdate group is needed, there is no layer competition
-    between the windows anymore, each windows has its own.
-
-    - the coordinates of the widgets contained in the windows are relative to
-    the window itself, not the screen.  It just makes things easier by reducing
-    maintenance when a window is moved.
+    * since the window has its own surface we make sure we never draw outside
+      of the window ;
+    * when a LayeredUpdate group is needed, there is no layer competition
+      between the windows anymore: each windows has its own.
+    * the coordinates of the widgets contained in the windows are relative to
+      the window itself, not the screen.  It makes things easier by reducing
+      maintenance when a window is moved.
 
     """
 
     BG_COLOR = (235, 235, 235)
-    SPRITES_GROUP_CLS = pygame.sprite.LayeredUpdates
 
     def __init__(self):
         GuiSprite.__init__(self)
-        self.sprites_group = self.SPRITES_GROUP_CLS()
+        self._sprites_group = pygame.sprite.LayeredUpdates()
+
+    def _drawSprites(self):
+        """Draw the contained sprites onto itself.
+
+        Warning: this DOES NOT call the method _draw of the sprites of the
+        _sprites_group.  It draws them as they are.
+
+        """
+
+        self._sprites_group.draw(self.drawable_image)
+
+    def _draw(self):
+        """Draw the window onto its own surface: background and sprites.
+
+        Warning: this DOES NOT call the method _draw of the sprites of the
+        _sprites_group.  It draws them as they are.
+
+        """
+        GuiSprite._draw(self)
+        self._drawSprites()
 
     def update(self):
-        """Refreshes the image of the window by redrawing all the sprites.
+        """(Re)create the sprite image from scratch.
+
+        Starts by calling update() on all the sprites of _sprites_group.
 
         Call this method only when you need it.  Not at every frame.
 
         """
-        self.sprites_group.update()
+        self._sprites_group.update()
         GuiSprite.update(self)
 
-    def _draw(self):
-        self._drawBackground()
-        self._drawSprites()
-
-    def _drawSprites(self):
-        self.sprites_group.draw(self.drawable_image)
-
     def getSpritesAt(self, pos):
+        """Return a list of all the contained sprites at pos, including self.
+
+        The sprites are returned from bottom to top.
+
+        """
         if not self.rect.collidepoint(pos):
             return []
         result = [self]
         rel_pos = (pos[0] - self.rect.left, pos[1] - self.rect.top)
-        sprites = self.sprites_group.get_sprites_at(rel_pos)
+        sprites = self._sprites_group.get_sprites_at(rel_pos)
         for sprite in sprites:
             result += sprite.getSpritesAt(rel_pos)
         return result
 
-    def _findDrawerOld(self):
-        return self
+    def moveToFront(self, sprite):
+        """Bring the provided sprite to the top of the top layer."""
+        self._sprites_group.move_to_front(sprite)
+
+    def moveToBack(self, sprite):
+        """Bring the provided sprite to the bottom of the bottom layer."""
+        self._sprites_group.move_to_back(sprite)
+
+    def addSprite(self, sprite, layer):
+        """Add the given sprite to the given layer of sprite group."""
+        if self._sprites_group.has(sprite):
+            msg = "Sprite already in group, cannot add twice."
+            raise SpriteAlreadyInGroupError(msg)
+        self._sprites_group.add(sprite, layer=layer)
+
+    def removeSprite(self, sprite):
+        """Remove the given sprite from the window sprite group."""
+        if not self._sprites_group.has(sprite):
+            msg = "Cannot remove a sprite that is not in the group."
+            raise SpriteNotInGroupError(msg)
