9a9ba37b97d55c133e8cf778a8ee4d4b3de2d874
Niriel <daneel_libre@yahoo.fr>
2011-01-07 13:26:34 +0100

    GuiSprite: more in-line documentation, and updated unit test.

--- src/pynguin/sprite/guisprite.py
+++ src/pynguin/sprite/guisprite.py
@@ -1,42 +1,28 @@
-'''
+"""
 Created on Nov 25, 2010
 
 @author: Niriel
-'''
+"""
 
 import pygame
 
 __all__ = ['GuiSprite']
 
 class GuiSprite(pygame.sprite.Sprite):
+    """Ancestor of all sprite using the PYnGUIn system.
+    
+    This class is abstract.  Please use its descendants.
+
+    """
     SURFACE_FLAGS = 0
     BG_COLOR = (32, 32, 32, 255)
 
     def __init__(self):
+        """Initialize a new GuiSprite."""
         pygame.sprite.Sprite.__init__(self)
         self.image = None
         self.rect = pygame.Rect(0, 0, 0, 0)
         self.drawable_image = self.image
-
-    def update(self):
-        """Redraw the sprite.
-
-        This method does two things:
-        1) call _createImage if there is no image or if the size of the image
-           does not match the size of the attribute rect.
-        2) call drawBackgound.
-
-        Overload this method to suit your need.  Do not forget to include a
-        call to this method when you overload in order to take advantage of the
-        point 1) explained above.  Note that you can also simply overload
-        _drawBackground if that's enough for you.
-
-        Call this method only when you need it.  Not at every frame.
-
-        """
-        if not self.image or self.image.get_size() != self.rect.size:
-            self._createImage()
-        self._draw()
 
     def _createImage(self):
         """Create a new pygame.Surface object for this sprite.
@@ -45,15 +31,52 @@ class GuiSprite(pygame.sprite.Sprite):
         must be up-to-date.
 
         """
+        # pylint: disable-msg=E1123,E1121
         self.image = pygame.Surface(self.rect.size, flags=self.SURFACE_FLAGS)
         self.drawable_image = self.image
 
     def _drawBackground(self):
-        """Fill the image property with the solid color BG_COLOR."""
+        """Fill the drawable_image attribute with the solid color BG_COLOR."""
         self.drawable_image.fill(self.BG_COLOR)
 
     def _draw(self):
+        """Draw this sprite.
+
+        This method is likely to be overridden in subclasses.
+
+        """
         self._drawBackground()
 
+    def update(self):
+        """(Re)create the sprite image from scratch.
+
+        This method does two things:
+
+        1. call _createImage if there is no image or if the size of the image
+           does not match the size of the attribute rect.
+        2. call _draw.
+
+        Overload this method to suit your needs (for example containers should
+        call update on their children first).  Do not forget to include a call
+        to this method when you overload in order to take advantage of the
+        point 1) explained above.  Note that you can also simply overload _draw
+        if that's enough for you.
+
+        Call this method only when you need it.  Not at every frame.
+
+        """
+        if not self.image or self.image.get_size() != self.rect.size:
+            self._createImage()
+        self._draw()
+
     def getSpritesAt(self, pos):
+        """Return [itself] if the given position is within rect.  Otherwise [].
+
+        This function returns a list in order to be compatible with drawer
+        sprites: sprites that contain and draw other sprites onto themselves
+        (like a window).  In that case, the window has to return itself and
+        also return the button at the position pos.  We end up with a list of
+        all the widgets, ordered by depth.
+
+        """
         return [self] if self.rect.collidepoint(pos) else []
--- src/pynguin/sprite/tests/guisprite_test.py
+++ src/pynguin/sprite/tests/guisprite_test.py
@@ -1,44 +1,105 @@
-'''
+"""
 Created on Nov 25, 2010
 
 @author: Niriel
-'''
+"""
 import unittest
 import pygame
-from gui.layout import size
-from gui.sprite import guisprite
+from pynguin.sprite import guisprite
+
+# pylint: disable-msg=R0904
+# Because unit tests have tons of public methods and that's normal. 
+
+# pylint: disable-msg=W0212
+# Disabling the warning about calling a 'protected' member of the class (name
+# starting with underscore).  I don't care, I'm allowed to do that in a test
+# suite.
 
 class TestGuiSprite(unittest.TestCase):
+    """Test the guisprite module."""
     def testGuiSpriteInit(self):
         """GuiSprite.__init__ does its job."""
-        s = guisprite.GuiSprite()
-        self.assertEquals(s.rect, pygame.Rect(0,0,0,0))
-        self.assertEquals(s.image, None)
+        sprite = guisprite.GuiSprite()
+        self.assertEquals(sprite.rect, pygame.Rect(0, 0, 0, 0))
+        self.assertEquals(sprite.image, None)
+        self.assertTrue(sprite.drawable_image is sprite.image)
+        self.assertEquals(sprite.SURFACE_FLAGS, 0)
+        self.assertEquals(sprite.BG_COLOR, (32, 32, 32, 255))
 
     def testCreateImage(self):
         """GuiSprite._createImage allocates a Surface with the right size."""
-        sp = guisprite.GuiSprite()
-        sp.rect.size = (8, 16)
-        sp._createImage()
-        image = sp.image
-        self.assertEquals(image.get_size(), (8, 16))
+        sprite = guisprite.GuiSprite()
+        sprite.rect.size = (8, 16)
 
-    def testAdjustRect(self):
-        """GuiSprite.adjustRect adapts the rect to the allocated_size."""
-        s = guisprite.GuiSprite()
-        s.rect = pygame.Rect(0, 0, 0, 0)
-        s.allocated_size = size.SizeAllocation(10, 20, 30, 40)
-        s.adjustRect()
-        self.assertEquals(s.rect, pygame.Rect(10, 20, 30, 40))
+        sprite._createImage()
+        self.assertTrue(sprite.drawable_image is sprite.image)
+        image = sprite.image
+        self.assertEquals(image.get_size(), (8, 16))
 
     def testGetSpritesAt(self):
         """GuiSprite.getSpritesAt returns [self] if pos within, otherwise [].
+
+        """
+        sprite = guisprite.GuiSprite()
+        sprite.rect = pygame.Rect(100, 200, 50, 50)
+        self.assertEquals(sprite.getSpritesAt((0, 0)), [])
+        self.assertEquals(sprite.getSpritesAt((125, 225)), [sprite])
+
+    def testDrawBackground(self):
+        """GuiSprite._drawBackground fills the surface with the bg color."""
+        sprite = guisprite.GuiSprite()
+        sprite.rect = pygame.Rect(100, 200, 50, 50)
+        sprite._createImage()
+        self.assertEquals(sprite.image.get_at((25, 25)), (0, 0, 0))
+        sprite._drawBackground()
+        self.assertEquals(sprite.image.get_at((25, 25)), sprite.BG_COLOR)
+
+    def testDraw(self):
+        """GuiSprite._draw calls _drawBackground."""
+        sprite = guisprite.GuiSprite()
+        sprite.rect = pygame.Rect(100, 200, 50, 50)
+        sprite._createImage()
+        self.assertEquals(sprite.image.get_at((25, 25)), (0, 0, 0))
+        sprite._draw()
+        self.assertEquals(sprite.image.get_at((25, 25)), sprite.BG_COLOR)
+
+    def testUpdateFromNone(self):
+        """GuiSprite.update calls _createImage if image is None."""
+        sprite = guisprite.GuiSprite()
+        sprite.rect = pygame.Rect(100, 200, 50, 50)
+        self.assertTrue(sprite.image is None)
+        sprite.update()
+        self.assertFalse(sprite.image is None)
+    
+    def testUpdateFromOtherSize(self):
+        """GuiSprite.update calls _createImage if image has wrong size."""
+        sprite = guisprite.GuiSprite()
+        sprite.rect = pygame.Rect(100, 200, 50, 50)
+        sprite._createImage()
+        image_before = sprite.image
+        sprite.rect = pygame.Rect(100, 200, 100, 50)
+        sprite.update()
+        image_after = sprite.image
+        self.assertFalse(image_before is image_after)
+
+    def testUpdateFromSameSize(self):
+        """GuiSprite.update doesn't call _createImage if image has same size.
         
         """
-        sp = guisprite.GuiSprite()
-        sp.rect = pygame.Rect(100, 200, 50, 50)
-        self.assertEquals(sp.getSpritesAt((0, 0)), [])
-        self.assertEquals(sp.getSpritesAt((125, 225)), [sp])
+        sprite = guisprite.GuiSprite()
+        sprite.rect = pygame.Rect(100, 200, 50, 50)
+        sprite._createImage()
+        image_before = sprite.image
+        sprite.update()
+        image_after = sprite.image
+        self.assertTrue(image_before is image_after)
+    
+    def testUpdateDraw(self):
+        """GuiSprite.update calls _draw."""
+        sprite = guisprite.GuiSprite()
+        sprite.rect = pygame.Rect(100, 200, 50, 50)
+        sprite.update()
+        self.assertEquals(sprite.image.get_at((25, 25)), sprite.BG_COLOR)
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
