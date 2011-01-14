d24e0b9b7456fb5eed6e1bbb6b5ac75d929b394a
Niriel <daneel_libre@yahoo.fr>
2011-01-14 23:25:59 +0100

    New visual-inspection test script for new widget system.

--- src/pynguin/test.py
+++ src/pynguin/test.py
@@ -1,69 +1,43 @@
-'''
+"""
 Created on Nov 29, 2010
 
 @author: Niriel
-'''
+"""
 
 import pygame
-from gui.layout import Cell
-from gui.widget import Screen, Label, HBox, VBox, Scroll, Button, TextBox
-from gui.widget import windowmanager
+from pynguin.layout import size
+from pynguin.widget import *
 
 EVENT_QUIT = pygame.QUIT
 
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
+SCREEN_WIDTH = 640
+SCREEN_HEIGHT = 480
 
 def main():
     pygame.init()
 
     pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
     font = pygame.font.Font(None, 30)
-    font_big = pygame.font.Font(None, 50)
+    #font_big = pygame.font.Font(None, 50)
 
-    screen = Screen()
-    window = Scroll()
-    vbox = VBox(16, True)
-    hbox = HBox(16, True)
-    label0 = Label(font, "Hello")
-    label1 = Label(font_big, "super happy")
-    button = Button()
-    label2 = Label(font, "world!")
-    textbox = TextBox(font)
-    screen.addChild(window)
-    window.addChild(vbox, Cell.EXPAND_PADDING, Cell.EXPAND_PADDING, 8)
-    vbox.addChild(label0, Cell.EXPAND_PADDING, Cell.EXPAND_PADDED)
-    vbox.addChild(hbox, Cell.EXPAND_PADDING, Cell.EXPAND_PADDING)
-    hbox.addChild(label1, Cell.EXPAND_PADDING, Cell.EXPAND_PADDING)
-    hbox.addChild(button, Cell.EXPAND_PADDING, Cell.EXPAND_PADDING)
-    button.addChild(label2, Cell.EXPAND_PADDING, Cell.EXPAND_PADDING, 8)
-    vbox.addChild(textbox, Cell.EXPAND_PADDED, Cell.EXPAND_PADDING, 8)
-
-    textbox._text = "sadjlpfADF"
-    textbox._drawText = textbox._drawTextEdit
-
-    screen.negotiateSize()
-    print window.allocated_size
-    windowmanager.ResizeToFitInto(window, screen)
-
-    button.setMode(Button.MODE_INACTIVE)
-    screen.update()
-    import random
-
+    screen = ScreenWidget()
+    box = VBoxWidget(0, True)
+    label_hello = LabelWidget(font, "Hello,")
+    label_world = LabelWidget(font,  "world !")
+    screen.addChild(box, 'end', 'padded', 'padded')
+    box.addChild(label_hello, 'end', 'padding', 'padding')
+    box.addChild(label_world, 'end', 'padded', 'padded')
+    screen.dispatchDisplayers()
+    screen.negotiateSize(True)
+    screen._sprite.update()
+    for cell in box.cells:
+        print cell.allocated_size
     running = True
     clock = pygame.time.Clock()
-    x, dx = 1, 1
     while running:
-        screen.batchUpdate(False)
         pygame.display.flip()
-        screen.batchUpdate(True)
-        clock.tick()
-        button.setMode(random.randrange(4))
-        label1.setText("%i FPS" % int(round(clock.get_fps())))
-        window.tryScrollTo(x, 0)
-        x += dx
-        if x >= 300 or x <= 0:
-            dx = -dx
+        clock.tick(100)
         for event in pygame.event.get():
             if event.type == EVENT_QUIT:
                 running = False
