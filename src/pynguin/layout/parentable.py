b62ee114a623fb80da8152928a723a7885c559f6
Niriel <daneel_libre@yahoo.fr>
2011-03-17 20:42:05 +0100

    Removed my fancy and useless WeakRef.

--- src/pynguin/layout/parentable.py
+++ src/pynguin/layout/parentable.py
@@ -4,19 +4,19 @@ Created on Nov 29, 2010
 @author: Niriel
 """
 
-from common.weakrefplus import WeakRef
+import weakref
 
 __all__ = ['ParentableError', 'NoParentError', 'AlreadyParentError',
            'Parentable']
 
 class ParentableError(RuntimeError):
-    pass
+    """Base error for the parentable module."""
 
 class NoParentError(ParentableError):
-    pass
+    """Parentable has no parent."""
 
 class AlreadyParentError(ParentableError):
-    pass
+    """Parentable already has a parent."""
 
 class Parentable(object):
     """Inherit from this class objects you want to put in a container."""
@@ -31,9 +31,16 @@ class Parentable(object):
 
         """
         object.__init__(self)
-        self._parent = WeakRef(None)
+        self._parent = None
     def _getParent(self):
+        """De-reference the weak reference to the parent, if any."""
+        if self._parent is None:
+            return None
         return self._parent()
     def _setParent(self, parent):
-        self._parent.set(parent)
+        """Create a weakref to the parent, or set _parent to None."""
+        if parent is None:
+            self._parent = None
+        else:
+            self._parent = weakref.ref(parent)
     parent = property(_getParent, _setParent)
